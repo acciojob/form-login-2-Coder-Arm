6a4a0cefa08eb71816d15eaf88eed26d53d0f7ef
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-04-11 12:06:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function AlertFunction(){
+	var x1 = document.getElementById('text1').value;
+	var x2 =  document.getElementById('text2').value;
+	var x3 =  document.getElementById('num').value;
+	var x4 =  document.getElementById('mail').value;
+	
+	alert("First Name: "+x1+"\n Last Name: "+x2+"\n Phone Number: "+x3+"\n Email ID: "+x4);
+}
